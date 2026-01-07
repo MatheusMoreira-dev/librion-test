2f422a82bc15d811e8eb5d883bcb4ee62fa112fe
Matheus <matheus.moreira.marcelino.dev@gmail.com>
2026-01-07 12:59:27 -0300

    adicionando comentário

--- routers/books_router.py
+++ routers/books_router.py
@@ -5,26 +5,32 @@ from services import BookService
 from schemas import SearchBook
 from exceptions.book_exception import BookNotFoundError
 
+# Roteador para operações com livros
 books_router = APIRouter(prefix="/books", tags=["books"])
 
+# Retorna uma lista de livros limitadas por um cursor (Por qual id iniciar?) e o size (Quantidade de itens)
 @books_router.get("/")
 async def get_books(cursor:int = Query(1, ge=1), size:int = Query(10, le=50), session:Session = Depends(get_session)):
     """Get books limit by cursor and size"""
     return BookService.list_books(cursor, size, session)
 
+# Rota para filtrar livros
 @books_router.post("/search")
 async def filter_books(filters:SearchBook, session:Session = Depends(get_session)):
     """Search using a combination of filters."""
     return BookService.filter_books(filters, session)
 
+# Rota para buscar um livro pelo seu Id
 @books_router.get("/{book_id}")
 async def get_book_by_id(book_id:int, session:Session = Depends(get_session)):
     """Get book by id"""
     try:
         return BookService.get_by_id(book_id, session)
+    
     except BookNotFoundError:
         raise HTTPException(status_code=404)
 
+#Rota para buscar as cópias registradas de um livro
 @books_router.get("/{book_id}/copies")
 async def get_copies(book_id:int, session:Session = Depends(get_session)):
     """Get copies of a book"""
